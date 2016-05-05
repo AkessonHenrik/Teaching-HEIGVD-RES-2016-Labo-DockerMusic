4408764d3d2ca92e0093ebd7f403a5a792f6112e
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-05-05 17:23:30 +0200

    App works, not through docker yet
    You can start an Auditor instance and musician instances that will
    communicate
    If an instrument hasn't played in 5 seconds, the Auditor removes it from
    its array.
    If you open a connection to the auditor's ip and port 2205, the auditor
    sends the current instruments, and closes the connection

--- docker/image-auditor/Auditor.js
+++ docker/image-auditor/Auditor.js
@@ -1,22 +1,73 @@
 /**
- * Created by Henrik on 04.05.2016.
- */
+* Created by Henrik on 04.05.2016.
+*/
+var net = require('net');
 
 var dgram = require('dgram');
 var udp4;
 var s = dgram.createSocket('udp4');
+udpInfo = {
+  'ip': '239.255.22.5',
+  'port': '9907'
+};
+tcpInfo = {
+  'ip':'239.255.22.5',
+  'port':'2205'
+};
 function Auditor() {
-    this.info = {
-        'ip': '239.255.22.5',
-        'port': '2205'
-    };
-    Auditor.prototype.callMusicians = function () {
-        var payload = JSON.stringify(this.info);
-        message = new Buffer(payload);
-        s.send(message, 0, message.length, 9907, "239.255.22.5", function () {});
-    };
-    this.interval = setInterval(this.callMusicians.bind(this), 500);
+  var tcpServer = net.createServer(function(socket) {
+    var arr = []
+    instrumentArray.forEach(function(instr) {
+      var obj = {
+        'uuid': instr.uuid,
+      'instrument': instr.instrument,
+      'activeSince': instr.activeSince
+    }
+      arr.push(obj)
+    })
+    socket.write(JSON.stringify(arr));
+    socket.pipe(socket);
+    socket.destroy();
+  });
+  tcpServer.listen(2205, '127.0.0.1');
+
+  instrumentArray = [];
+
+  var s = dgram.createSocket('udp4');
+  s.bind(udpInfo.port, function () {
+    s.addMembership(udpInfo.ip);
+  });
+
+
+  s.on('message', function (msg, source) {
+    var obj = JSON.parse(msg);
+    var toAdd = true;
+    for (var i = 0; i < instrumentArray.length; i++) {
+      if (instrumentArray[i].uuid === obj.uuid) {
+        instrumentArray[i].lastActive = new Date();
+        console.log("Got: " + JSON.stringify(instrumentArray[i]));
+        toAdd = false;
+        break;
+      }
+    }
+    if (toAdd) {
+      obj.lastActive = new Date();
+      obj.activeSince = obj.lastActive;
+      instrumentArray.push(obj);
+      console.log("Added new instrument: " + JSON.stringify(obj));
+    }
+  });
+
+  Auditor.prototype.check = function () {
+    instrumentArray.forEach(function(instrument) {
+      var d = new Date();
+      if((d-instrument.lastActive)/1000 > 5) {
+        instrumentArray.splice(instrumentArray.indexOf(instrument), 1);
+        console.log("Removed! " + instrumentArray)
+      }
+    })
+  };
+  setInterval(this.check.bind(this), 1000);
 }
 
 var auditor = new Auditor();
-auditor.callMusicians();
--- docker/image-musician/Musician.js
+++ docker/image-musician/Musician.js
@@ -1,62 +1,41 @@
 /**
- * Created by Henrik on 04.05.2016.
- */
+* Created by Henrik on 04.05.2016.
+*/
 
 var dgram = require('dgram');
 var udp4;
 var s = dgram.createSocket('udp4');
 var uuidpackage = require('node-uuid');
 var sounds = [
-    {'instr': 'piano', 'sound': 'ti-ta-ti'},
-    {'instr': 'trumpet', 'sound': 'pouet'},
-    {'instr': 'flute', 'sound': 'trulu'},
-    {'instr': 'violin', 'sound': 'gzi-gzi'},
-    {'instr': 'drum', 'sound': 'boum-boum'}
+  {'instr': 'piano', 'sound': 'ti-ta-ti'},
+  {'instr': 'trumpet', 'sound': 'pouet'},
+  {'instr': 'flute', 'sound': 'trulu'},
+  {'instr': 'violin', 'sound': 'gzi-gzi'},
+  {'instr': 'drum', 'sound': 'boum-boum'}
 ];
 
 function Musician(instr) {
-    this.instrument = instr;
-    this.sound = null;
-    this.uuid = uuidpackage.v4();
-    this.tcpIp = null;
-    this.tcpPort = null;
-    for (var i = 0; i < sounds.length; i++) {
-        if (sounds[i].instr === instr) {
-            this.sound = sounds[i].sound;
-        }
+  this.instrument = instr;
+  this.sound = "";
+  this.uuid = uuidpackage.v4();
+  for (var i = 0; i < sounds.length; i++) {
+    if (sounds[i].instr === instr) {
+      this.sound = sounds[i].sound;
     }
-    Musician.prototype.findAuditor = function () {
-
-        //if we find an auditor we won't need to periodically look for auditors anymore
-        var found = false;
-        s.bind(9907, function() {
-            s.addMembership("239.255.22.5");
-        });
-        s.on('message', function(msg, source) {
-            msg = JSON.parse(msg);
-            this.tcpIp = msg.ip;
-            this.tcpPort = msg.port;
-            console.log("tcpIp = " + this.tcpIp + " and tcpPort = " + this.tcpPort);
-            found = true;
-        });
-        if(found === true) {
-            // clearInterval(this.interval);
-            this.interval = setInterval(this.play.bind(this), 1000);
-        }
+  }
+  Musician.prototype.play = function () {
+    var o = {
+      'uuid': this.uuid,
+      'instrument': this.instrument,
+      'sound': this.sound
     };
-    Musician.prototype.play = function () {
-        //Define tcp connection here
-
-        s.close();
-        var obj = {
-            "sound": this.sound
-        };
-        var payload = JSON.stringify(obj);
-        var message = new Buffer(payload);
-        console.log("Sending message: " + message);
-    };
-    this.findAuditor();
-    // this.interval = setInterval(this.findAuditor.bind(this), 100);
+    var payload = JSON.stringify(o);
+    var message = new Buffer(payload);
+    s.send(message, 0, message.length, 9907, "239.255.22.5", function () {
+      console.log("Playing: " + message);
+    });
+  }
+  setInterval(this.play.bind(this), 1000);
 }
 
 var instrument = process.argv[2];
